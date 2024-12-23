fb0b23928f30f12dcbc1e651afc533c79578918c
Micaelle Souza <61849864+micaelleos@users.noreply.github.com>
2024-12-23 14:52:00 +0000

    fixing identation

--- tools.py
+++ tools.py
@@ -15,66 +15,63 @@ from chromadb.config import Settings
 import streamlit as st
 import tempfile
 
-
-
-
 def get_youtube_transcription(video_url, language_code=['pt']):
-  # Extrair o ID do vídeo a partir do URL
-  video_id = video_url.split("v=")[-1]
-  if "&" in video_id:
-      video_id = video_id.split("&")[0]
-  # Obter a transcrição no idioma especificado
-  transcript = YouTubeTranscriptApi.get_transcript(video_id,languages=language_code)
-  return transcript
+    # Extrair o ID do vídeo a partir do URL
+    video_id = video_url.split("v=")[-1]
+    if "&" in video_id:
+        video_id = video_id.split("&")[0]
+    # Obter a transcrição no idioma especificado
+    transcript = YouTubeTranscriptApi.get_transcript(video_id,languages=language_code)
+    return transcript
 
 def format_transcript(transcript):
-  tempo = 0
-  formated_list = []
-  frase = ""
-  for t in transcript:
-    tempo += t['duration']
-    frase += t['text']
-    if tempo >= 300:
-      formated_list.append({"text":frase,"start":tempo})
-      tempo = 0
-      frase = ""
-    formated_list.append({"text":frase,"start":round(tempo)})
-  return formated_list
+    tempo = 0
+    formated_list = []
+    frase = ""
+    for t in transcript:
+      tempo += t['duration']
+      frase += t['text']
+      if tempo >= 300:
+        formated_list.append({"text":frase,"start":tempo})
+        tempo = 0
+        frase = ""
+      formated_list.append({"text":frase,"start":round(tempo)})
+    return formated_list
 
 def format_doc(docs,link):
-  formated_docs = []
-  for doc in docs:
-    link_tempo = link + "&t=" + str(doc["start"]) + "s"
-    document = Document(
-            page_content=doc['text'],
-            metadata={"source": link_tempo,"time":doc["start"]}
-        )
-    formated_docs.append(document)
-  return formated_docs
+    formated_docs = []
+    for doc in docs:
+      link_tempo = link + "&t=" + str(doc["start"]) + "s"
+      document = Document(
+              page_content=doc['text'],
+              metadata={"source": link_tempo,"time":doc["start"]}
+          )
+      formated_docs.append(document)
+    return formated_docs
 
 @st.cache_resource()
 def vector_store(id):
-  embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.environ["OPEN_API_KEY"])
-  vectorstore = InMemoryVectorStore(embeddings)
-  return vectorstore
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.environ["OPEN_API_KEY"])
+    vectorstore = InMemoryVectorStore(embeddings)
+    return vectorstore
 
 def load_doc_to_db(doc_splits):
-  embeddings = OpenAIEmbeddings(model="text-embedding-3-large",api_key=os.environ["OPEN_API_KEY"])
-  db = vector_store(st.session_state.id)
-  # Add to vectorDB
-  db.from_documents(embedding=embeddings,documents=doc_splits)
-  print("loaded")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large",api_key=os.environ["OPEN_API_KEY"])
+    db = vector_store(st.session_state.id)
+    # Add to vectorDB
+    db.from_documents(embedding=embeddings,documents=doc_splits)
+    print("loaded")
 
 def load_doc_pipeline(link,language_code='pt'):
-  print("inicio")
-  transcript = get_youtube_transcription(link)
-  print(transcript)
-  formated_list = format_transcript(transcript)
-  print(formated_list)
-  doc_splits = format_doc(formated_list,link)
-  print(doc_splits)
-  load_doc_to_db(doc_splits)
-  print("video loaded")
+    print("inicio")
+    transcript = get_youtube_transcription(link)
+    print(transcript)
+    formated_list = format_transcript(transcript)
+    print(formated_list)
+    doc_splits = format_doc(formated_list,link)
+    print(doc_splits)
+    load_doc_to_db(doc_splits)
+    print("video loaded")
 
 
 @tool(response_format="content_and_artifact")
